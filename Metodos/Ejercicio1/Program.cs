5d705b40f10447399fe6c5ca0e55ce416f7326de
Nagore <reina.nagore@gmail.com>
2019-10-02 15:31:15 +0200

    Ejercicio1 Completo

---
+++ Metodos/Ejercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribe una función que escriba “Hola” 3 veces. Llámala desde el programa principal.
+            SayHello();
+        }
+        public static void SayHello()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Hola");
+            }
+        }
+    }
+}
