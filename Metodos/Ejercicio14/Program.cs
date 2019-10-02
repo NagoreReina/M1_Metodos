1d815252ab57ea3ba6dd9409bf435bb1a6c2153c
Nagore <reina.nagore@gmail.com>
2019-10-02 18:49:24 +0200

    Creado el ejercicio 14

---
+++ Metodos/Ejercicio14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crea una aplicación que nos calcule el área de un círculo, cuadrado o triángulo. 
+               Pediremos de qué figura queremos calcular su área y según lo introducido pedirá los valores 
+               necesarios para calcular el área. Crea un método por cada figura para calcular cada área, 
+               este devolverá un número real. Muestra el resultado por pantalla
+                Circulo: (radio^2)*PI
+                Triángulo: (base * altura) / 2
+                Cuadrado: lado * lado
+             */
+
+        }
+    }
+}
